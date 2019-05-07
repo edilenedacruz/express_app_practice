6a564c06d5f3a62a23cd1aea297a4b4a86726fbe
Edilene <edilene_cruz@yahoo.com.br>
2019-05-07 09:51:29 -0600

    refactoring of app to apply MVC concepts

--- server.js
+++ server.js
@@ -1,126 +1,18 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
+const papers = require('./lib/routes/api/v1/papers');
+const footnotes = require('./lib/routes/api/v1/footnotes');
 
-const environment = process.env.NODE_ENV || 'development';
-const configuration = require('./knexfile')[environment];
-const database = require('knex')(configuration);
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Publications';
 
-app.get('/', (request, response) => {
-  response.send('Hello, Publications');
-});
+app.use('/api/v1/papers', papers);
+app.use('/api1/v1/footnotes', footnotes);
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}.`);
-});
-
-app.get('/api/v1/papers', (request, response) => {
-  database('papers').select()
-    .then((papers) => {
-      response.status(200).json(papers);
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
-
-app.get('/api/v1/footnotes', (request, response) => {
-  database('footnotes').select()
-    .then((footnotes) => {
-      response.status(200).json(footnotes);
-    })
-    .catch((error) => {
-      response.status(500).json({ error });
-    });
-});
-
-app.post('/api/v1/papers', (request, response) => {
-  const paper = request.body;
-
-  for (let requiredParameter of ['title', 'author', 'publisher']) {
-    if (!paper[requiredParameter]) {
-      return response
-      .status(422)
-      .send({ error: `Expected format: { title: <String>, author: <String> }. You're missing a "${requiredParameter}" property.`});
-    }
-  }
-
-  database('papers').insert(paper, 'id')
-    .then(paper => {
-      response.status(201).json({ id: paper[0] })
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-app.post('/api/v1/footnotes', (request, response) => {
-  const footnote = request.body;
-
-  for (let requiredParameter of ['note', 'paper_id']) {
-    if (!footnote[requiredParameter]) {
-      return response
-        .status(422)
-        .send({ error: `Expected format: { note: <String>, paper_id: <String> You're missig a ${requiredParameter} property.` });
-    }
-  }
-
-  database('papers').pluck('id')
-    .then(paper_ids => {
-      if (paper_ids.indexOf(parseInt(footnote.paper_id)) === -1) {
-        return response
-          .status(422)
-          .send({ error: 'The paper id you provided does not exist.' });
-      } else {
-        database('footnotes').insert(footnote, 'id')
-          .then(footnote => {
-            response.status(201).json({ id: footnote[0] })
-          })
-          .catch (error => {
-            response.status(500).json({ error });
-          });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-app.get('/api/v1/papers/:id', (request, response) => {
-  database('papers').where('id', request.params.id).select()
-    .then(papers => {
-      if (papers.length) {
-        response.status(200).json(papers);
-      } else {
-        response.status(404).json({
-          error: `Could not find paper with id ${request.params.id}`
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
-});
-
-app.get('/api/v1/papers/:id/footnotes', (request, response) => {
-  database('footnotes')
-    .where('footnotes.paper_id', request.params.id)
-    .select()
-    .then(footnotes => {
-      if (footnotes.length) {
-        response.status(200).json(footnotes);
-      } else {
-        response.status(404).json({
-          error: `Could not find footnotes for paper with id ${requet.params.id}`
-        });
-      }
-    })
-    .catch(error => {
-      response.status(500).json({ error });
-    });
 });
